b1d5b6e582cb6b94a37aeb908f11ae3fcfd957ad
SeonJungKim <ksj083895@gmail.com>
2021-07-12 16:44:45 +0900

    Alert 메세지에 문자열보간과 랜덤 값 넣기

--- MyFirstAlbum/ViewController.swift
+++ MyFirstAlbum/ViewController.swift
@@ -13,10 +13,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    var currentValue: Int = 0
 
 
     @IBAction func alertBtn(_ sender: Any) {
-        let alert = UIAlertController(title: "옵션", message: "옵션을 선택해주세요", preferredStyle: .actionSheet)
+        
+        let message = "이 제품의 가격은 $\(currentValue) 입니다."
+        
+        let alert = UIAlertController(title: "옵션", message: message, preferredStyle: .alert)
         
         alert.addAction(UIAlertAction(title: "추가", style: .default, handler: { UIAlertAction in
             print("추가 버튼 선택")
@@ -26,13 +31,16 @@ class ViewController: UIViewController {
             print("취소 버튼 선택")
         }))
         
-        alert.addAction(UIAlertAction(title: "확인", style: .destructive, handler: { UIAlertAction in
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: { UIAlertAction in
             print("확인 버튼 선택")
         }))
         
         self.present(alert, animated: true) {
             print("버튼을 클릭했습니다,")
         }
+        
+        let randomPrice = arc4random_uniform(100000) + 1
+        currentValue = Int(randomPrice)
     }
 }
 
