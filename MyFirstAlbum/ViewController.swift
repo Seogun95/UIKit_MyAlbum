9ed3d936edbc6ddab31afcdc4a65f1fb8c5bdc00
SeonJungKim <ksj083895@gmail.com>
2021-07-12 15:58:58 +0900

    UiKit 에서 Alert 버튼 구성

--- MyFirstAlbum/ViewController.swift
+++ MyFirstAlbum/ViewController.swift
@@ -15,5 +15,24 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func alertBtn(_ sender: Any) {
+        let alert = UIAlertController(title: "옵션", message: "옵션을 선택해주세요", preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "추가", style: .default, handler: { UIAlertAction in
+            print("추가 버튼 선택")
+        }))
+        
+        alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: { UIAlertAction in
+            print("취소 버튼 선택")
+        }))
+        
+        alert.addAction(UIAlertAction(title: "확인", style: .destructive, handler: { UIAlertAction in
+            print("확인 버튼 선택")
+        }))
+        
+        self.present(alert, animated: true) {
+            print("버튼을 클릭했습니다,")
+        }
+    }
 }
 
